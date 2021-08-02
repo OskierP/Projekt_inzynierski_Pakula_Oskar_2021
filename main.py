3199d19c2e2f468fd69803578c1230e4ae821449
OskarP <pakooskar@gmail.com>
2021-08-02 13:43:47 +0200

    Cos z disppersion range

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #Gdansk University of Technology
 #Bachelor of Science - project
 
-
+import numpy as np
 
 def file_2_list(file, x: str):
 
@@ -11,11 +11,15 @@ def file_2_list(file, x: str):
     return list
 
 
-
-
-def Disperssion_range(n: int, file):  #zakresy dyspersyjne w zależności od przyjetego modelu
+def Disperssion_range(n: int, epsilon, tau, alpha, freq: float):  #zakresy dyspersyjne w zależności od przyjetego modelu
+    sigma = 0
     for i in range(0, n): #zakres od 1 do n. Należy pamiętać, iż programy liczą od zera (tablice)
-        print('s')
+        sigma = complex(sigma + (epsilon[i]/(1+(1j*2*np.pi*freq*tau[i])**(1-alpha[i]))))
+
+
+def N_cole_cole(N: int, file_epsilon, file_tau, file_alpha, freq: float, epsiO, sig ):
+    result = 0
+    result = complex( Disperssion_range(N, file_epsilon, file_tau, file_alpha, freq) + (sig/(1j*2*np.pi*freq*epsiO)))
 
 
 def main():
