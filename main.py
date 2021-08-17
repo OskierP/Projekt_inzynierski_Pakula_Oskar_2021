66da019a6ecb85ede46f019410def5072369759f
OskierP <pakooskar@gmail.com>
2021-08-17 23:14:01 +0200

    Ver. 1.4.2, changed function name and optimized imports (temporary)

--- main.py
+++ main.py
@@ -1,47 +1,58 @@
-#Made by Oskar Pakula
-#Gdansk University of Technology
-#Bachelor of Science - project
+# Made by Oskar Pakula
+# Gdansk University of Technology
+# Bachelor of Science - project
 
-import numpy as np
-
-from Objects_2_Cole_Cole import Two_Cole_Cole_Objects as TCCO
-from Objects_4_Cole_Cole import FCC
+import constant as const
 from Objects_4_Cole_Cole import Four_Cole_Cole_Objects as FCCO
-from file_2_list import show_list
+from file_2_list import *
 
 
-def Disperssion_range(n: int, epsilon, tau, alpha, freq: float):  #zakresy dyspersyjne w zależności od przyjetego modelu
+def disperssion_range(n: int, epsilon, tau, alpha,
+                      freq: float):  # zakresy dyspersyjne w zależności od przyjetego modelu
     sigma = 0
 
-    for i in range(0, n): #zakres od 1 do n. Należy pamiętać, iż programy liczą od zera (tablice)
-        sigma = complex(sigma + (epsilon[i]/(1+(1j*2*np.pi*freq*tau[i])**(1-alpha[i]))))
+    for i in range(0, n):  # zakres od 1 do n. Należy pamiętać, iż programy liczą od zera (tablice)
+        sigma = complex(sigma + (epsilon[i] / (1 + (1j * 2 * const.PI * freq * tau[i]) ** (1 - alpha[i]))))
+
+    return sigma
 
 
-def N_cole_cole(N: int, material: int, freq_bottom: int, freq_upper: int, list):
-    result = 0
+def n_cole_cole(n: int, freq_bottom: int, freq_upper: int, list, step: int):
+    array = [['tissue']]
+    i = 1
 
-    for i in range(len(list)):
-        for j in range(freq_bottom, freq_upper):
+    for freqz in range(freq_bottom, freq_upper + step, step):
+        array[0].append(str(freqz))
 
+    for tissue in list:
+        array.append([])
+        array[i].append(tissue.get_name())
+        for freq in range(freq_bottom, freq_upper + step, step):
+            result = complex(tissue.get_epsilon_inf() + disperssion_range(n, tissue.get_epsilon(), tissue.get_tau(),
+                                                                          tissue.get_alpha(), float(freq)) + (
+                                     tissue.get_sigma0() / (1j * 2 * const.PI * const.epsilon_0)))
+            array[i].append(str(result))
+        i += 1
 
-            print('s')
-
-
-    result = complex( Disperssion_range(N, file_epsilon, file_tau, file_alpha, freq) + (sig/(1j*2*np.pi*freq*epsiO)))
+    return array
 
 
 def main():
+    freq_4_cc_bottom = 1 * 10 ** 6
+    freq_4_cc_upper = 10 * 10 ** 9
+    step = 10000 * 10 ** 6
+
     # CC4 = file_2_list(open('Cole-Cole_4.csv', 'r'), ",") # 4-Cole-cole: czytanie watości z pliku csv
     # CC2 = file_2_list(open('Cole-Cole_2.csv', 'r'), ",") # 2-Cole-cole: czytanie watości z pliku csv
 
     # print(np.array(CC4))
     # print('\n')
     # print(np.array(CC2))
-    print(FCCO[0].get_tau())
-    print(TCCO[1].get_name())
-    print(FCCO[1].get_sigma())
-    print(FCCO[1].get_epsilon_inf())
-    print(show_list(FCC[1]))
+    # print(FCCO[0].get_tau())
+    # print(TCCO[1].get_name())
+    # print(FCCO[1].get_sigma0())
+    # print(FCCO[1].get_epsilon_inf())
+    # print(show_list(FCC))
     # print(epsilon(CC2,0,2))
     # print(tau(CC2,0,2))
     # print(alpha(CC2,0,2))
@@ -49,11 +60,13 @@ def main():
     # x=complex((1+2/1j))
     # y=4
     # z= complex(x+y)
-    #print(z.imag)
+    # print(z.imag)
+
+    list_2_file(n_cole_cole(4, freq_4_cc_bottom, freq_4_cc_upper, FCCO, step), 'testowy4.csv')
 
 
 if __name__ == '__main__':
     np.set_printoptions(linewidth=150)
     main()
 
-#test czy działą
+# test czy działą
