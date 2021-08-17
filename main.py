682c5cdc946024bc637e39d5950257e4f19d0812
OskierP <pakooskar@gmail.com>
2021-08-17 15:06:08 +0200

    Posredni , backup

--- main.py
+++ main.py
@@ -57,18 +57,26 @@ def alpha(list, index: int, n: int):
 
 def Disperssion_range(n: int, epsilon, tau, alpha, freq: float):  #zakresy dyspersyjne w zależności od przyjetego modelu
     sigma = 0
+
     for i in range(0, n): #zakres od 1 do n. Należy pamiętać, iż programy liczą od zera (tablice)
         sigma = complex(sigma + (epsilon[i]/(1+(1j*2*np.pi*freq*tau[i])**(1-alpha[i]))))
 
 
-def N_cole_cole(N: int, file_epsilon, file_tau, file_alpha, freq: float, epsiO, sig ):
+def N_cole_cole(N: int, material: int, freq_bottom: float, freq_upper: float, list: []):
     result = 0
+
+    for i in range(len(list)):
+        for j in range(freq_bottom, freq_upper):
+            print('s')
+
+
     result = complex( Disperssion_range(N, file_epsilon, file_tau, file_alpha, freq) + (sig/(1j*2*np.pi*freq*epsiO)))
 
 
 def main():
     CC4 = file_2_list(open('Cole-Cole_4.csv', 'r'), ",") # 4-Cole-cole: czytanie watości z pliku csv
     CC2 = file_2_list(open('Cole-Cole_2.csv', 'r'), ",") # 2-Cole-cole: czytanie watości z pliku csv
+
 
     # print(np.array(CC4))
     # print('\n')
@@ -86,4 +94,5 @@ def main():
 if __name__ == '__main__':
     np.set_printoptions(linewidth=150)
     main()
+
 #test czy działą
