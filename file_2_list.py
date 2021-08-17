7e01cc1f948688e48190c89a18aabe95748751bf
OskierP <pakooskar@gmail.com>
2021-08-17 18:14:27 +0200

    Transfered function that trasnfer file to list to seperate file

---
+++ file_2_list.py
@@ -0,0 +1,17 @@
+def file_2_list(file, x: str):
+    Array = []
+    Array.append([])
+    index = 0
+
+    for element in file.read().split(f"{x}"):
+
+        if '\n' in element:
+            tmp = element.split('\n')
+            Array[index].append(tmp[0])
+            Array.append([])
+            index += 1
+            Array[index].append(tmp[1])
+        else:
+            Array[index].append(element)
+
+    return Array
