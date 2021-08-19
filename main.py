53214fa97cf2a0477939576721a7a575ea7b3adb
OskarP <pakooskar@gmail.com>
2021-08-19 14:59:02 +0200

    ver. 1.4.5

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 # Made by Oskar Pakula
 # Gdansk University of Technology
 # Bachelor of Science - project
+import csv
 
 import constant as const
 from Objects_2_Cole_Cole import Two_Cole_Cole_Objects as TCCO
 from Objects_4_Cole_Cole import Four_Cole_Cole_Objects as FCCO
-from file_functions import *
+from file_functions_module import *
+from plot_module import plot
 
 
 def disperssion_range(n: int, epsilon, tau, alpha, freq: float):  # zakresy dyspersyjne
@@ -18,58 +20,59 @@ def disperssion_range(n: int, epsilon, tau, alpha, freq: float):  # zakresy dysp
 
 
 def n_cole_cole(n: int, freq_bottom: int, freq_upper: int, list, step: int):
-    array = [['tissue']]
-    i = 1
-
-    for freqz in range(freq_bottom, freq_upper + step, step):
-        array[0].append(str(freqz))
+    array_tissue = []
+    i = 0
 
     for tissue in list:
-        array.append([])
-        array[i].append(tissue.get_name())
+        array_tissue.append([])
+        array_tissue[i].append(tissue.get_name())
+
         for freq in range(freq_bottom, freq_upper + step, step):
             result = complex(tissue.get_epsilon_inf() +
                              disperssion_range(
                                  n, tissue.get_epsilon(), tissue.get_tau(), tissue.get_alpha(), float(freq)
                              ) +
                              (tissue.get_sigma0() / (1j * 2 * const.PI * const.epsilon_0)))
-            array[i].append(str(round(result.real, 3)))
+            array_tissue[i].append(str(round(result.real, 3)))
         i += 1
 
-    return array
+    return array_tissue
+
+
+def freq_for_plot(freq_bottom: int, freq_upper: int, step: int, files):
+    array = ['frequency']
+
+    for freqz in range(freq_bottom, freq_upper + step, step):
+        array.append(freqz)
+
+    with open(files, 'w') as file:
+        for element in array:
+            file.write(f'{element},')
+    file.close()
 
 
 def main():
-    freq_4_cc_bottom = 1 * 10 ** 6
-    freq_4_cc_upper = 10 * 10 ** 9
-    step = 100 * 10 ** 6
+    freq_bottom = 1 * 10 ** 6
+    freq_upper = 26500 * 10 ** 6
+    step = 1 * 10 ** 6
 
-    freq_2_cc_bottom = 500 * 10 ** 6
-    freq_2_cc_upper = 26500 * 10 ** 6
+    # calculates values epsilon_r
+    # list_2_file(n_cole_cole(4, freq_bottom, freq_upper, FCCO, step), 'testowy4.csv')
+    # list_2_file(n_cole_cole(2, freq_bottom, freq_upper, TCCO, step), 'testowy2.csv')
+    # freq_for_plot(freq_bottom, freq_upper, step, 'frequency.csv') #need to delete last ","
 
-    # CC4 = file_2_list(open('Cole-Cole_4.csv', 'r'), ",") # 4-Cole-cole: czytanie watości z pliku csv
-    # CC2 = file_2_list(open('Cole-Cole_2.csv', 'r'), ",") # 2-Cole-cole: czytanie watości z pliku csv
+    # calculates values sigma
+    #
+    #
 
-    # print(np.array(CC4))
-    # print('\n')
-    # print(np.array(CC2))
-    # print(FCCO[0].get_tau())
-    # print(TCCO[1].get_name())
-    # print(FCCO[1].get_sigma0())
-    # print(FCCO[1].get_epsilon_inf())
-    # print(show_list(FCC))
-    # print(FCCO[1].get_tau())
-    # print(epsilon(CC2,0,2))
-    # print(tau(CC2,0,2))
-    # print(alpha(CC2,0,2))
-    # Disperssion_range(4, plik)
-    # x=complex((1+2/1j))
-    # y=4
-    # z= complex(x+y)
-    # print(z.imag)
+    freq = file_2_dict('frequency.csv', ',')
+    four_cole_cole_dict = file_2_dict('testowy4.csv', ',')
+    two_cole_cole_dict = file_2_dict('testowy2.csv', ',')
 
-    list_2_file(n_cole_cole(4, freq_4_cc_bottom, freq_4_cc_upper, FCCO, step), 'testowy4.csv')
-    list_2_file(n_cole_cole(2, freq_2_cc_bottom, freq_2_cc_upper, TCCO, step), 'testowy2.csv')
+    # print(two_cole_cole_dict['frequency'].index(500000000))
+
+    # Does not metter if we take four_cole_cole_dict or two_cole_cole_dict for frequency
+    plot(freq['frequency'], four_cole_cole_dict['Dry_Skin'], two_cole_cole_dict['Skin'])
 
 
 if __name__ == '__main__':
